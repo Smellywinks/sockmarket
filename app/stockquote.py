5dae3fd76f5cf85ae6174b610de7655b6e7b21f5
Mario Minyon <mariominyon@gmail.com>
2017-01-28 18:49:43 -0500

    updated stock quote reference id

--- app/stockquote.py
+++ app/stockquote.py
@@ -4,7 +4,7 @@ import re
 def get_quote(symbol):
     base_url = 'http://finance.google.com/finance?q='
     content = urllib.urlopen(base_url + symbol).read()
-    m = re.search('id="ref_694653_l".*?>(.*?)<', content)
+    m = re.search('id="ref_304466804484872_l".*?>(.*?)<', content)
     if m:
         quote = m.group(1)
     else:
